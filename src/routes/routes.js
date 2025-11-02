4ea9ef28161948db40e4cbdcb9bc356d8679f90b
Nogrilo <nogriloesirjeferson@gmail.com>
2025-11-02 15:08:06 -0300

    tirando o desnecessario do codigo

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,11 +5,12 @@ const CategoriaDAO = require("../DAO/CategoriaDAO");
 const LivroDAO = require("../DAO/LivroDAO");
 
 const FilmeDAO = require("../DAO/FilmeDAO");
-
+/*
 router.post('/categorias', CategoriaDAO.criar)
 router.get('/categorias', CategoriaDAO.listar)
 router.post('/categorias/deletar/:id', CategoriaDAO.delete)
 router.post('/categorias/alterar/:id', CategoriaDAO.alterarPorId)
+*/
 
 router.post('/filmes/criar',        FilmeDAO.criar)
 router.get('/filmes/listar',        FilmeDAO.listar)
