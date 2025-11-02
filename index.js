bae97c24997e4d9edffcad5baf6eccc2255a81b1
Nogrilo <nogriloesirjeferson@gmail.com>
2025-11-02 14:44:22 -0300

    alterando o arquivo index.js

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+
+const Filme = require('./src/models/Filme.js')
+const FilmeDAO = require('./src/DAO/FilmeDAO.js')
+
+/*
 const Categoria = require('./src/models/Categoria.js');
 const CategoriaDAO = require('./src/DAO/CategoriaDAO.js');
+*/
+
 const app = express();
-const categoriaRoutes = require('./src/routes/routes.js');
+//const categoriaRoutes = require('./src/routes/routes.js');
 
 app.use(express.json()); // <-- tem que vir antes das rotas!
 app.use('/api', categoriaRoutes);
