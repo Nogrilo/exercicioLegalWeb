87cf3ccbb90c6bb3431f41b6a7e206f671cc7a5c
Nogrilo <nogriloesirjeferson@gmail.com>
2025-11-02 14:31:31 -0300

    Criando a classe Filme e adicionando os getters e setters

---
+++ src/models/Filme.js
@@ -0,0 +1,53 @@
+class Filme{
+    titulo = ""
+    
+    sinopse = ""
+
+    genero = ""
+
+    duracao = 0
+
+    diretor = ""
+
+    getTitulo() {
+        return this.titulo
+    }
+    setTitulo(titulo) {
+        this.titulo = titulo
+    }
+    getSinopse() {
+        return this.sinopse
+    }
+    setSinopse(sinopse) {
+        this.sinopse = sinopse
+    }
+
+    getGenero() {
+        return this.genero
+    }
+
+    setGenero(genero) {
+        this.genero = genero
+    }
+
+    getDuracao() {
+        return this.duracao
+    }
+
+    setDuracao(duracao) {
+        this.duracao = duracao
+    }
+
+    getDiretor() {
+        return this.diretor
+    }
+
+    setDiretor(diretor) {
+        this.diretor = diretor
+    }
+    constructor(){
+
+    }
+
+}
+module.exports = Categoria
