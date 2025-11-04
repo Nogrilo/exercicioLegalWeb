aed7396494bff66cb9c58c37b92dc4ec44db1a5a
Nogrilo <nogriloesirjeferson@gmail.com>
2025-11-04 19:11:07 -0300

    adicionando a classe Genero

---
+++ src/models/Genero.js
@@ -0,0 +1,27 @@
+class Genero{
+    nome = ""
+    
+    descricao = ""
+
+    getNome() {
+        return this.nome
+    }
+
+    setNome(nome){
+        this.nome = nome
+    }
+
+    getDescricao(){
+        return this.descricao
+    }
+
+    setDescricao(){
+        this.descricao = this.descricao
+    }
+
+    constructor(){
+
+    }
+
+}
+module.exports = Genero
