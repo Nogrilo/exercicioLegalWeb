5b5088e539978c536d958bf62a7b409f37c92321
Nogrilo <nogriloesirjeferson@gmail.com>
2025-11-04 19:19:43 -0300

    alterando o arquivo de rotas

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,10 +2,16 @@ const express   = require("express")
 const router    = express.Router();
 
 const FilmeDAO  = require("../DAO/FilmeDAO");
+const GeneroDAO = require("../DAO/GeneroDAO");
+const Genero = require("../models/Genero");
 
 router.post('/filmes/criar',        FilmeDAO.criar)
 router.get('/filmes/listar',        FilmeDAO.listar)
 router.post('/filmes/alterar/:id',  FilmeDAO.alterarPorId)
 router.post('/filmes/deletar/:id',  FilmeDAO.deletar)
 
+router.post('/generos/criar',       Genero.criar)
+router.get('/generos/listar',       GeneroDAO.listar)
+router.post('/generos/alterar/:id', GeneroDAO.alterarPorId)
+router.post('/generos/deletar/:id', GeneroDAO.deletar)
 module.exports = router
