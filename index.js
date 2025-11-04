6a64aad166e233281b3976cc1fb612ad36a62a99
Nogrilo <nogriloesirjeferson@gmail.com>
2025-11-03 21:31:09 -0300

    identando o codigo

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express       = require('express');
+const express       = require('express'); // Importar o express
 
-const Filme         = require('./src/models/Filme.js')
-const FilmeDAO      = require('./src/DAO/FilmeDAO.js')
+const Filme         = require('./src/models/Filme.js') // Importar classe Filme
+const FilmeDAO      = require('./src/DAO/FilmeDAO.js') // Importar classe FilmeDAO
 
 const app           = express();
-const filmeRoutes   = require('./src/routes/routes.js')
+const filmeRoutes   = require('./src/routes/routes.js') // Rota
 
 app.use(express.json());
 app.use('/api', filmeRoutes);
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,16 +1,7 @@
-const express = require("express")
-const router = express.Router();
+const express   = require("express")
+const router    = express.Router();
 
-const CategoriaDAO = require("../DAO/CategoriaDAO");
-const LivroDAO = require("../DAO/LivroDAO");
-
-const FilmeDAO = require("../DAO/FilmeDAO");
-/*
-router.post('/categorias', CategoriaDAO.criar)
-router.get('/categorias', CategoriaDAO.listar)
-router.post('/categorias/deletar/:id', CategoriaDAO.delete)
-router.post('/categorias/alterar/:id', CategoriaDAO.alterarPorId)
-*/
+const FilmeDAO  = require("../DAO/FilmeDAO");
 
 router.post('/filmes/criar',        FilmeDAO.criar)
 router.get('/filmes/listar',        FilmeDAO.listar)
