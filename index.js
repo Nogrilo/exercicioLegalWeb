a55f2e87e29f8ed7158e3aa94567179562c4bb7f
Nogrilo <nogriloesirjeferson@gmail.com>
2025-11-06 21:28:40 -0300

    alterando coisas legais!

--- index.js
+++ index.js
@@ -12,6 +12,43 @@ const filmeRoutes   = require('./src/routes/routes.js') // Rota
 app.use(express.json());
 app.use('/api', filmeRoutes);
 
+
 const PORT = 3000;
 app.use(express.static('public'));
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+
+// teste para rodar o front
+
+// handlebars
+app.set("view engine", "handlebars")
+app.use("/", filmeRoutes)
+
+const path          = require('path')
+const basePath      = path.join(__dirname, 'templates')
+app.use(express.static(basePath))
+
+app.use(
+    express.urlencoded({
+        extended: true
+    }),
+)
+
+app.use(express.json())
+
+app.get('/api/', (req, res) => {
+    res.sendFile(`${basePath}/index.html`)
+})
+
+app.get('/filmes', (req, res) => {
+    res.sendFile(`${basePath}/filme.html`)
+})
+
+app.get('/genero', (req, res) => {
+    res.sendFile(`${basePath}/genero.html`)
+    console.log(req.body);
+})
+
+app.post("/genero.html", (req, res) => {
+  const { nome, descricao } = req.body;
+  GeneroDAO.criar(req, res)
+});
